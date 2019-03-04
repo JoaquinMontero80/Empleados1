7d5695ac1c0b93205b68dae4e47f226236f1e398
Joaquín <descartes.1980@gmail.com>
2019-03-04 10:33:11 +0100

    Hola Fran, encantado de volverte a ver

---
+++ Empleados1/src/Empleados1.java
@@ -0,0 +1,61 @@
+public class Empleados1 {
+	private int id;
+	private String nombre;
+	private String apellidos;
+	private String dirección;
+	private int teléfono;
+	
+	public Empleados1(int id, String nombre, String apellidos, String dirección, int teléfono) {
+		this.id=id;
+		this.nombre=nombre;
+		this.apellidos=apellidos;
+		this.dirección=dirección;
+		this.teléfono=teléfono;
+		
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getDirección() {
+		return dirección;
+		
+	}
+
+	public void setDirección(String dirección) {
+		this.dirección = dirección;
+	}
+
+	public int getTeléfono() {
+		return teléfono;
+	}
+
+	public void setTeléfono(int teléfono) {
+		this.teléfono = teléfono;
+	}
+
+	
+	
+}
